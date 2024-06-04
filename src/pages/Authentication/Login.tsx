1c97b7654513a6440db790f4fbd830ad21e7ab5b
Sasanka Roy <sasankaroy033@gmail.com>
2024-06-04 15:23:26 +0530

    login page design changes

--- src/pages/Authentication/Login.tsx
+++ src/pages/Authentication/Login.tsx
@@ -1,94 +1,104 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import { logIn, verifyToken } from '../../api-calls/apicalls';
-import { useNavigate } from "react-router-dom";
-import logoDark from "../../assets/logo-dark.png"
-import darkBg from "../../assets/dark_pattern.png"
-import "../../css/login.css"
+import { useNavigate } from 'react-router-dom';
+import logoDark from '../../assets/logo-dark.png';
+import darkBg from '../../assets/dark_pattern.png';
+import '../../css/login.css';
 
 function Login() {
-  const [showNotFound, setShowNotFound] = useState<boolean | undefined>(undefined)
-  const [password, setPassword] = useState<string>()
-  const [email, setEmail] = useState<string>()
+  const [showNotFound, setShowNotFound] = useState<boolean | undefined>(
+    undefined,
+  );
+  const [password, setPassword] = useState<string>();
+  const [email, setEmail] = useState<string>();
 
   const navigate = useNavigate();
 
   const handleLogin = async () => {
-    const loginData = await logIn({ email: email, password: password })
+    const loginData = await logIn({ email: email, password: password });
     // console.log("+++",loginData)
-    if (loginData.success == "no") {
-      alert(loginData?.message)
-    } else if (loginData.success == "yes") {
+    if (loginData.success == 'no') {
+      alert(loginData?.message);
+    } else if (loginData.success == 'yes') {
       // console.log("login successfull")
       // setLoginValidity(true)
-      localStorage.setItem("token", loginData.token)
-      navigate("/dashboard");
+      localStorage.setItem('token', loginData.token);
+      navigate('/dashboard');
     }
-  }
+  };
 
   useEffect(() => {
-
-
     const verifier = async () => {
-
-      if (localStorage.getItem("token")) {
-        const verifiedTokenData = await verifyToken()
+      if (localStorage.getItem('token')) {
+        const verifiedTokenData = await verifyToken();
         // console.log("rrr",verifiedTokenData?.message)
-        if (verifiedTokenData?.message == "jwt expired") {
-          setShowNotFound(false)
+        if (verifiedTokenData?.message == 'jwt expired') {
+          setShowNotFound(false);
         } else {
-          setShowNotFound(true)
+          setShowNotFound(true);
         }
       } else {
-        setShowNotFound(false)
+        setShowNotFound(false);
       }
-    }
+    };
 
-    verifier()
-
+    verifier();
   }, []);
 
   if (showNotFound === true) {
     return (
-      <div className='flex justify-center'>
-        You are already logged in, go to&nbsp;<a className="underlined" href="/dashboard">dashboard</a>
+      <div className="flex justify-center">
+        You are already logged in, go to&nbsp;
+        <a className="underlined" href="/dashboard">
+          dashboard
+        </a>
       </div>
     );
   } else if (showNotFound === false) {
     return (
-      <div className="flex justify-center items-center h-screen" style={{ backgroundImage: `url(${darkBg})`, backgroundSize: "cover" }}>
-        <div className='admin__login_container'>
+      <div
+        className="flex justify-center items-center h-screen"
+        style={{ backgroundImage: `url(${darkBg})`, backgroundSize: 'cover' }}
+      >
+        <div className="admin__login_container flex flex-col justify-center items-center gap-4 h-[50%] bg-[#161313] rounded-2xl w-[40%]">
           <div className="flex justify-center mt-3">
-            <img alt="dark-logo" src={logoDark} className='w-25 h-25' />
+            <img alt="dark-logo" src={logoDark} className="w-25 h-25" />
           </div>
-          <h2 className="text-center mt-3 text-white">Admin Login</h2>
-          <div className='flex flex-col'>
-            <div className="mt-5 mb-3 flex justify-center">
+          <div className="flex flex-col  w-[70%] mx-auto">
+            <h2 className="text-center text-2xl font-[600] mt-3 text-white">
+              Admin Login
+            </h2>
+            <div className="mt-5 mb-3 flex justify-center w-full">
               <input
                 type="email"
-                className='rounded'
-                placeholder='Enter email'
+                className="rounded-lg bg-[#202020] text-lg w-full py-2 px-3 border border-[#fff6] text-[#ccc] outline-none"
+                placeholder="Enter email"
                 id="email"
                 name="email"
                 value={email}
                 autoComplete="off"
-                onChange={(e) => { setEmail(e.target.value) }}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
               />
             </div>
             <div className="mt-3 mb-3 flex justify-center ">
               <input
-                type="text"
-                className='rounded'
-                placeholder='Enter password'
+                type="password"
+                className="rounded-lg bg-[#202020] text-lg w-full py-2 px-3 border border-[#fff6] text-[#ccc] outline-none"
+                placeholder="Enter password"
                 id="password"
                 name="password"
                 value={password}
                 autoComplete="off"
-                onChange={(e) => { setPassword(e.target.value) }}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
               />
             </div>
-            <div className='mt-3 mb-5 flex justify-center '>
+            <div className="mt-3 mb-5 flex justify-center ">
               <button
-                className="bg-cyan-500 hover:bg-cyan-600 w-25 rounded text-white"
+                className="bg-blue-700 hover:bg-blue-600 w-[25%] rounded-lg py-2 text-lg font-[500] transition-all duration-200 ease-in-out text-white"
                 onClick={handleLogin}
               >
                 Login
@@ -99,7 +109,6 @@ function Login() {
       </div>
     );
   }
-
 }
 
-export default Login
+export default Login;
