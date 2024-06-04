31143d3b5bab03d5309b6c9c9e3a486148f5a800
Sasanka Roy <sasankaroy033@gmail.com>
2024-06-04 18:26:48 +0530

    sidebar changes

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -105,7 +105,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
 
             <ul className="mb-6 flex flex-col gap-1.5">
               {/* <!-- Menu Item Dashboard --> */}
-              <SidebarLinkGroup
+              {/* <SidebarLinkGroup
                 activeCondition={
                   pathname === '/' || pathname.includes('dashboard')
                 }
@@ -171,7 +171,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                           />
                         </svg>
                       </NavLink>
-                      {/* <!-- Dropdown Menu Start --> */}
+                      
                       <div
                         className={`translate transform overflow-hidden ${
                           !open && 'hidden'
@@ -199,6 +199,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                             >
                               User Management
                             </NavLink>
+
                           </li>
                           <li>
                             <NavLink
@@ -222,6 +223,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                               MCQ Template Editor
                             </NavLink>
                           </li>
+
                           <li>
                             <NavLink
                               to="/quiz-template-editor"
@@ -313,11 +315,11 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                           </li>
                         </ul>
                       </div>
-                      {/* <!-- Dropdown Menu End --> */}
+                      
                     </React.Fragment>
                   );
                 }}
-              </SidebarLinkGroup>
+              </SidebarLinkGroup> */}
               {/* <!-- Menu Item Dashboard --> */}
 
               {/* <!-- Menu Item Calendar --> */}
@@ -351,33 +353,150 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
 
               {/* <!-- Menu Item Profile --> */}
 
-              {/* <li>
+
+              <li>
                 <NavLink
-                  to="/profile"
+                  to="/dashboard"
                   className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname.includes('profile') && 'bg-graydark dark:bg-meta-4'
+                    pathname.includes('dashboard') && 'bg-graydark dark:bg-meta-4'
                   }`}
                 >
-                  <svg
-                    className="fill-current"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M9.0002 7.79065C11.0814 7.79065 12.7689 6.1594 12.7689 4.1344C12.7689 2.1094 11.0814 0.478149 9.0002 0.478149C6.91895 0.478149 5.23145 2.1094 5.23145 4.1344C5.23145 6.1594 6.91895 7.79065 9.0002 7.79065ZM9.0002 1.7719C10.3783 1.7719 11.5033 2.84065 11.5033 4.16252C11.5033 5.4844 10.3783 6.55315 9.0002 6.55315C7.62207 6.55315 6.49707 5.4844 6.49707 4.16252C6.49707 2.84065 7.62207 1.7719 9.0002 1.7719Z"
-                      fill=""
-                    />
-                    <path
-                      d="M10.8283 9.05627H7.17207C4.16269 9.05627 1.71582 11.5313 1.71582 14.5406V16.875C1.71582 17.2125 1.99707 17.5219 2.3627 17.5219C2.72832 17.5219 3.00957 17.2407 3.00957 16.875V14.5406C3.00957 12.2344 4.89394 10.3219 7.22832 10.3219H10.8564C13.1627 10.3219 15.0752 12.2063 15.0752 14.5406V16.875C15.0752 17.2125 15.3564 17.5219 15.7221 17.5219C16.0877 17.5219 16.3689 17.2407 16.3689 16.875V14.5406C16.2846 11.5313 13.8377 9.05627 10.8283 9.05627Z"
-                      fill=""
-                    />
-                  </svg>
-                  Profile
+                  
+                  Dashboard
                 </NavLink>
-              </li> */}
+              </li>
+
+              <li>
+                <NavLink
+                  to="/user-management"
+                  className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                    pathname.includes('user') && 'bg-graydark dark:bg-meta-4'
+                  }`}
+                >
+                  
+                  User Management
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/file-template-editor"
+                  className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                    pathname.includes('file') && 'bg-graydark dark:bg-meta-4'
+                  }`}
+                >
+                  
+                  File Template Editor
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/mcq-template-editor"
+                  className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                    pathname.includes('mcq') && 'bg-graydark dark:bg-meta-4'
+                  }`}
+                >
+                  
+                  MCQ Template Editor
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/quiz-template-editor"
+                  className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                    pathname.includes('quize') && 'bg-graydark dark:bg-meta-4'
+                  }`}
+                >
+                  
+                  Quiz Template Editor
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/gallery-management"
+                  className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                    pathname.includes('gallery') && 'bg-graydark dark:bg-meta-4'
+                  }`}
+                >
+                  
+                  Gallery Management
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/product-management"
+                  className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                    pathname.includes('product') && 'bg-graydark dark:bg-meta-4'
+                  }`}
+                >
+                  
+                  Product Management
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/category-management"
+                  className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                    pathname.includes('category') && 'bg-graydark dark:bg-meta-4'
+                  }`}
+                >
+                  
+                  Category Management
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/training-module-management"
+                  className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                    pathname.includes('training') && 'bg-graydark dark:bg-meta-4'
+                  }`}
+                >
+                  
+                  Training Module Management
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/choose-us"
+                  className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                    pathname.includes('choose-us') && 'bg-graydark dark:bg-meta-4'
+                  }`}
+                >
+                  
+                  Choose Us Management
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/partner-management"
+                  className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                    pathname.includes('partner') && 'bg-graydark dark:bg-meta-4'
+                  }`}
+                >
+                  
+                  Partners Management
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/service-management"
+                  className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                    pathname.includes('service') && 'bg-graydark dark:bg-meta-4'
+                  }`}
+                >
+                  
+                  Services Management
+                </NavLink>
+              </li>
+              
 
               {/* <!-- Menu Item Profile --> */}
 
